4fd28e6fcfdcedc30e8184546c472ab292f11385
daniil.paderin <daniil.paderin.dev@gmail.com>
2021-02-13 18:34:53 +0200

    Add Polaris

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function FirstPost(): JSX.Element {
-  return <h1>Hello world</h1>;
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+function FirstPost(): JSX.Element {
+  return (
+    <Page>
+      <Layout>
+        <EmptyState
+          heading="Manage your inventory transfers"
+          action={{ content: 'Select products' }}
+          image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+        >
+          <p>Select products</p>
+        </EmptyState>
+      </Layout>
+    </Page>
+  );
 }
+
+export default FirstPost;
